59558519371da29ac467af469646734544bfc551
CircleCI <no-reply@circleci.com>
2026-06-04 16:13:33 +0000

    chore: Update formula for version v0.21.0

--- ContainerHive.rb
+++ ContainerHive.rb
@@ -1,18 +1,18 @@
 class Containerhive < Formula
   desc "Swarm it. Build it. Run it"
   homepage "https://github.com/ContainerHive/ContainerHive"
-  version "0.20.1"
+  version "0.21.0"
 
   depends_on "zstd" => :build
 
   on_macos do
     on_arm do
-      url "https://github.com/ContainerHive/ContainerHive/releases/download/v0.20.1/darwin-arm64.tar.zst"
-      sha256 "f886d60086f4dc9bbca56bffadeae1223da37150fcfd5f793df78672bf4c7777"
+      url "https://github.com/ContainerHive/ContainerHive/releases/download/v0.21.0/darwin-arm64.tar.zst"
+      sha256 "b299d08f39d9e1f142ac62c8ac46419834be3469d4f74aba27eb566c9fe7986d"
     end
     on_intel do
-      url "https://github.com/ContainerHive/ContainerHive/releases/download/v0.20.1/darwin-amd64.tar.zst"
-      sha256 "6134cca13d68d921da134978de1ec88f7d9b7e22aec52eef1ba67584699abdb4"
+      url "https://github.com/ContainerHive/ContainerHive/releases/download/v0.21.0/darwin-amd64.tar.zst"
+      sha256 "a4120aa1c4a25fbc2268160c361005dc80217a7f3b72a7bc0f11a4a71e0bc7c1"
     end
   end
 
